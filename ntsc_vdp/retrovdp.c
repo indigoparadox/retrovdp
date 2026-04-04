49416ce73e6c548ba4c2fcae5b12585a1920d98a
indigoparadox <indigo.repo@interfinitydynamics.info>
2026-04-04 18:57:01 -0400

    Fixes to work with current VDP and commonize makefile.

--- ntsc_vdp/retrovdp.c
+++ ntsc_vdp/retrovdp.c
@@ -42,12 +42,12 @@ MERROR_RETVAL retroflat_vdp_init( struct RETROFLAT_STATE* state ) {
    crt_init(
       &(data->crt), state->screen_v_w, state->screen_v_h,
 #if defined( RETROFLAT_API_SDL1 )
-      CRT_PIX_FORMAT_RGBA, state->buffer.surface->pixels );
+      CRT_PIX_FORMAT_RGBA, state->platform.buffer.surface->pixels );
 #elif defined( RETROFLAT_API_SDL2 )
       CRT_PIX_FORMAT_RGBA, NULL );
 #else
       /* TODO */
-      CRT_PIX_FORMAT_BGRA, state->buffer.bits );
+      CRT_PIX_FORMAT_BGRA, state->platform.buffer.bits );
 #endif /* RETROFLAT_API_SDL1 */
    data->crt.blend = 1;
    data->crt.scanlines = 1;
@@ -84,7 +84,7 @@ MERROR_RETVAL retroflat_vdp_flip( struct RETROFLAT_STATE* state ) {
    struct VDP_DATA* data = (struct VDP_DATA*)(state->vdp_data);
 
 #if defined( RETROFLAT_API_SDL1 ) || defined( RETROFLAT_API_SDL2 )
-   assert( 4 == state->buffer.surface->format->BytesPerPixel );
+   assert( 4 == state->platform.buffer.surface->format->BytesPerPixel );
 #endif /* RETROFLAT_API_SDL1 */
    assert( 0 < state->screen_v_w );
    assert( 0 < state->screen_v_h );
@@ -93,13 +93,13 @@ MERROR_RETVAL retroflat_vdp_flip( struct RETROFLAT_STATE* state ) {
 
 #if defined( RETROFLAT_API_SDL1 ) || defined( RETROFLAT_API_SDL2 )
    data->ntsc.data = state->vdp_buffer->surface->pixels;
-   data->crt.out = state->buffer.surface->pixels;
+   data->crt.out = state->platform.buffer.surface->pixels;
    data->ntsc.format = CRT_PIX_FORMAT_RGBA;
 #else
    assert( NULL != state->vdp_buffer->bits );
 
    data->ntsc.data = state->vdp_buffer->bits;
-   data->crt.out = state->buffer.bits;
+   data->crt.out = state->platform.buffer.bits;
    data->ntsc.format = CRT_PIX_FORMAT_BGRA;
 #endif /* RETROFLAT_API_SDL1 */
 
